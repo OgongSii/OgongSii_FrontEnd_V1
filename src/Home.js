d2731ba27d58e8c42fa9ba8c463b3e1ff8dd65e5
박상현 <104070112+Sanghyun0505@users.noreply.github.com>
2022-11-23 20:11:34 +0900

    nav태그 추가

--- src/Home.js
+++ src/Home.js
@@ -8,19 +8,21 @@ const Li=styled.li`
 export default function Home(){
     return(
         <div>
-            <div className="position">
-                <Li>
-                    <Link to='./timer' className="line" target='_blank'>타이머</Link>
-                </Li>
-                <Li>   
-                    <Link to='./rank' className="line">랭킹</Link>
-                </Li>
-                <Li>
-                    <Link to='./login' className="line">로그인</Link>
-                </Li>
-                <Li>
-                    <Link to='./SignUp' className="line">회원가입</Link>
-                </Li>
+            <div>
+                <nav className="position">
+                    <Li>
+                        <Link to='./timer' className="line" target='_blank'>타이머</Link>
+                    </Li>
+                    <Li>   
+                        <Link to='./rank' className="line">랭킹</Link>
+                    </Li>
+                    <Li>
+                        <Link to='./login' className="line">로그인</Link>
+                    </Li>
+                    <Li>
+                        <Link to='./SignUp' className="line">회원가입</Link>
+                    </Li>
+                </nav>
             </div>
         </div>
     );
